d3d364a185e7b36c53b1bd3f899ef7b6225cc6b2
Joseph Pecoraro <jpecoraro342@yahoo.com>
2023-01-24 19:03:02 -0500

    Working application with only caffeinate command

--- Caffeine Menu Bar/Caffeine.swift
+++ Caffeine Menu Bar/Caffeine.swift
@@ -7,6 +7,86 @@
 
 import Cocoa
 
-class Caffeine: NSObject {
+class Caffeine {
+    var statusItem: NSStatusItem!
+    var caffeinateProcess: Process?
     
+    init() {
+        refreshUI()
+    }
+    
+    func updateMenu() {
+        let menu = NSMenu()
+        
+        let title = caffeinateProcess == nil ? "Caffeinate" : "Stop Caffeinating"
+
+        let caffeinateMenuItem = NSMenuItem(title: title, action: #selector(toggleCaffeination), keyEquivalent: "")
+        caffeinateMenuItem.target = self
+        
+        menu.addItem(caffeinateMenuItem)
+        menu.addItem(NSMenuItem.separator())
+        
+        // TODO: Add more menu items
+        // Caffeinate for 1 hour
+        // Caffeinate for 3 hours
+        // Caffeinate for 12 hours
+        // Display sleep?
+        // others?
+        
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "Q"))
+
+        statusItem.menu = menu
+    }
+    
+    func updateStatusIcon() {
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        
+        let imageName = "cup.and.saucer.fill"
+        statusItem.button?.image = NSImage(systemSymbolName: imageName, accessibilityDescription: "Toggle Wake State")
+        
+        // TODO: change this to a "hot coffee" when caffeine is active
+    }
+    
+    func refreshUI() {
+        updateStatusIcon()
+        updateMenu()
+    }
+    
+    @objc func toggleCaffeination() {
+        if let caffeine = caffeinateProcess {
+            caffeine.interrupt()
+            caffeinateProcess = nil
+        } else {
+            caffeinate()
+        }
+        
+        refreshUI()
+    }
+    
+    func caffeinate() {
+        // TODO: make the caffeinate process a builder (and add test cases)
+        let caffeinate = Process()
+        caffeinate.launchPath = "/usr/bin/caffeinate"
+        
+        do {
+            try caffeinate.run()
+            caffeinateProcess = caffeinate
+        } catch {
+            displayAlert(error: error)
+        }
+    }
+    
+    func displayAlert(error: Error) {
+        let alert = NSAlert()
+        alert.messageText = "Unable to Caffeinate"
+        alert.informativeText = error.localizedDescription
+        alert.addButton(withTitle: "Ok")
+        alert.alertStyle = .warning
+        alert.runModal()
+    }
+    
+    func suspendTasks() {
+        caffeinateProcess?.interrupt()
+        caffeinateProcess = nil
+    }
 }
--- Caffeine Menu Bar/main.swift
+++ Caffeine Menu Bar/main.swift
@@ -12,3 +12,17 @@ let delegate = AppDelegate()
 NSApplication.shared.delegate = delegate
 
 _ = NSApplicationMain(CommandLine.argc, CommandLine.unsafeArgv)
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var caffeine: Caffeine?
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        caffeine = Caffeine();
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        caffeine?.suspendTasks()
+    }
+    
+    // TODO: cleanup background processes on SIGKILL
+}
